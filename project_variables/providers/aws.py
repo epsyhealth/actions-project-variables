d1005956ec873ecc12a488abd72c6615fddfa94a
Daniel Ancuta <whisller@gmail.com>
2021-09-09 16:37:58 +0100

    Add `stage_local` variable.

--- project_variables/providers/aws.py
+++ project_variables/providers/aws.py
@@ -1,21 +1,31 @@
+STAGES = {
+    "pull_request": "playground",
+    "manual": "playground",
+    "push": "development",
+    "release_created": "staging",
+    "release_published": "production",
+}
+
+ENV_NAME_TO_SHORT = {
+    "playground": "play",
+    "development": "dev",
+    "production" : "prod",
+}
+
+
 class AWSProvider:
     def is_enabled(self):
         return True
 
     def dump(self, variables):
-        stages = {
-            "pull_request": "playground",
-            "manual": "playground",
-            "push": "development",
-            "release_created": "staging",
-            "release_published": "production",
-        }
-
         local = {}
         stage = variables.get("stage")
-        if "stage" not in variables:
-            stage = stages.get(variables.get("workflow", "manual"))
+        if not stage:
+            stage = STAGES.get(variables.get("workflow", "manual"))
             local["stage"] = stage
+
+        if "stage_short" not in variables:
+            local["stage_short"] = ENV_NAME_TO_SHORT.get(stage, stage)
 
         local["aws_account_key"] = f"AWS_{stage}_ACCOUNT_ID".upper()
 
